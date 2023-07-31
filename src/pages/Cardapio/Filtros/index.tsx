782d2bbf6a698cd03d39285ad54bfc5bfde6c9bb
Gabriel Carvalho <gabriel225gc@gmail.com>
2023-07-31 15:32:46 -0300

    criando os filtros

---
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -0,0 +1,21 @@
+import filtros from './filtros.json';
+import styles from './Filtros.module.scss';
+
+type IOpcao = typeof filtros[0];
+
+export default function Filtros() {
+  function selecionarFiltro(opcao:IOpcao) {}
+  return (
+    <div className={styles.filtros}>
+      {filtros.map((opcao) => (
+        <button 
+          key={opcao.id} 
+          onClick={() => selecionarFiltro(opcao)}
+          className={styles.filtros__filtro}
+        >
+          {opcao.label}
+        </button>
+      ))}
+    </div>
+  )
+}
