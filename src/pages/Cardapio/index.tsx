627c7d3edf0c396ed39ac4a148200654d94bee9e
Gabriel Carvalho <gabriel225gc@gmail.com>
2023-07-31 18:35:02 -0300

    estrutura do ordenador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -3,6 +3,7 @@ import logo from '../../assets/logo.svg';
 import Buscador from './Buscador';
 import { useState } from 'react';
 import Filtros from './Filtros';
+import Ordenador from './Ordenador';
 
 export default function Cardapio () {
   const [busca, setBusca] = useState("");
@@ -24,6 +25,7 @@ export default function Cardapio () {
         <Buscador busca={busca} setBusca={setBusca}/>
         <div className={styles.cardapio__filtros}>
           <Filtros filtro={filtro} setFiltro={setFiltro}/>
+          <Ordenador/>
         </div>
       </section>
     </main>
