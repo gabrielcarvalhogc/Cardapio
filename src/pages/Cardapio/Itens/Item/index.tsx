169563aa6c97510c1d10e34a0d725904d017cc29
Gabriel Carvalho <gabriel225gc@gmail.com>
2023-08-01 18:42:55 -0300

    itens sendo renderizados dinamicamente

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,22 +1,30 @@
-import Logo from '../../../../assets/logo.svg';
 import styles from './Item.module.scss';
+import cardapio from '../itens.json';
+import classNames from 'classnames';
 
-export default function Item() {
+type Props = typeof cardapio[0];
+
+export default function Item(props: Props) {
+  const { title, description, category, size, serving, price, photo } = props;
+
   return (
     <div className={styles.item}>
       <div className={styles.item__imagem}>
-        <img src={Logo} alt="imagem"/>
+        <img src={photo} alt={title}/>
       </div>
       <div className={styles.item__descricao}>
         <div className={styles.item__titulo}>
-          <h2>Macarrao</h2>
-          <p>Descricao do macarrao</p>
+          <h2>{title}</h2>
+          <p>{description}</p>
         </div>
         <div className={styles.item__tags}>
-          <div className={styles.item__tipo}>massa</div>
-          <div className={styles.item__porcao}>400g</div>
-          <div className={styles.item__qtdpessoas}>Serve 2 pessoas</div>
-          <div className={styles.item__valor}>R$ 50</div>
+          <div className={classNames({
+            [styles.item__tipo]: true,
+            [styles[`item__tipo__${category.label.toLocaleLowerCase()}`]]: true
+          })}>{category.label}</div>
+          <div className={styles.item__porcao}>{size}g</div>
+          <div className={styles.item__qtdpessoas}>Serve {serving} pessoa{serving === 1 ? '' : 's'}</div>
+          <div className={styles.item__valor}>R$ {price.toFixed(2)}</div>
         </div>
       </div>
     </div>
