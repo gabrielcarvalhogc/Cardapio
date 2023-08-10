1625a78a63583f209eed00e43a66b39aff3219fa
Gabriel Carvalho <gabriel225gc@gmail.com>
2023-08-10 14:29:22 -0300

    nova seção da página de inicio

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,6 +1,7 @@
 import cardapio from '../../data/cardapio.json';
 import styles from './Inicio.module.scss';
 import stylesTema from '../../styles/Tema.module.scss';
+import nossaCasa from '../../assets/nossa_casa.png';
 
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
@@ -17,6 +18,14 @@ export default function Inicio() {
             <button className={styles.recomendado__botao}>Ver mais</button>
           </div>
         ))}
+      </div>
+      <h3 className={stylesTema.titulo}>Nossa casa</h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="Nossa casa" />
+        <div className={styles.nossaCasa__endereco}>
+          Rua Vergueiro, 3145 <br /><br />
+          Vila Mariana - SP
+        </div>
       </div>
     </section>
   );
